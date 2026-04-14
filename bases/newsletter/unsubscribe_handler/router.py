cf6f78e6357c68db50d7d16f9bd145fa21aa98a6
ycalk <alosev752@gmail.com>
2026-04-14 23:26:06 +0500

    feat(unsubscribe): добавление ответа после отписки от канала

--- bases/newsletter/unsubscribe_handler/router.py
+++ bases/newsletter/unsubscribe_handler/router.py
@@ -24,18 +24,31 @@ async def unsubscribe(
         return templates.TemplateResponse(
             request=request,
             name="404.html",
+            status_code=404,
         )
     return templates.TemplateResponse(
         request=request,
-        name="confirm.html",
+        name="unsubscribe.html",
     )
 
 
 @router.post("/unsubscribe/{subscription_id}")
 async def unsubscribe_confirm(
     subscription_id: UUID,
+    request: Request,
     newsletter_subscription_dao: FromDishka[NewsletterSubscriptionDAO],
+    templates: FromDishka[Jinja2Templates],
 ):
     subscription = await newsletter_subscription_dao.find_by_id(subscription_id)
     if subscription is not None:
         await newsletter_subscription_dao.delete(subscription_id)
+        await newsletter_subscription_dao.commit()
+        return templates.TemplateResponse(
+            request=request,
+            name="confirm.html",
+        )
+    return templates.TemplateResponse(
+        request=request,
+        name="404.html",
+        status_code=404,
+    )
